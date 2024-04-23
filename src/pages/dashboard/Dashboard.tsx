1a49f53a92eaf7f499e73efffcb49ae114e0f638
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-04-23 12:43:55 +0300

    feat: input

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,89 +1,50 @@
-import Grid from '@mui/material/Grid';
-import { useApiUrl, useCustom, useTranslate } from '@refinedev/core';
-import dayjs from 'dayjs';
-import React, { useMemo, useState } from 'react';
-import { Card, RefineListView } from '../../components';
+import { Autocomplete, Button, Card, CardContent, CardHeader, Stack, TextField } from '@mui/material';
+import React from 'react';
+import usePlacesService from 'react-google-autocomplete/lib/usePlacesAutocompleteService';
+import { RefineListView } from '../../components';
 import { DashboardMap } from '../../components/dashboard';
-import { IOrderChart, ISalesChart } from '../../interfaces';
 import { DashboardWeather } from './Weather';
 
-type DateFilter = 'lastWeek' | 'lastMonth';
-
-const DATE_FILTERS: Record<
-  DateFilter,
-  {
-    text: string;
-    value: DateFilter;
-  }
-> = {
-  lastWeek: {
-    text: 'lastWeek',
-    value: 'lastWeek',
-  },
-  lastMonth: {
-    text: 'lastMonth',
-    value: 'lastMonth',
-  },
-};
-
 export const DashboardPage: React.FC = () => {
-  const t = useTranslate();
+  const { placePredictions, getPlacePredictions, isPlacePredictionsLoading } = usePlacesService({
+    apiKey: import.meta.env.VITE_MAPS_API_KEY,
+    debounce: 200,
+  });
 
   return (
     <RefineListView>
       <DashboardWeather />
-      <Grid container columns={24} spacing={3}>
-        <Grid
-          item
-          xs={24}
-          sm={24}
-          md={24}
-          lg={15}
-          xl={15}
-          sx={{
-            height: '504px',
-          }}>
-          <Card
-            title={t('dashboard.deliveryMap.title')}
-            cardContentProps={{
-              sx: {
-                height: '424px',
-              },
-            }}>
-            <DashboardMap />
-          </Card>
-        </Grid>
-        <Grid
-          item
-          xs={24}
-          sm={24}
-          md={24}
-          lg={9}
-          xl={9}
-          sx={{
-            height: '504px',
-          }}>
-          <Card title={t('dashboard.timeline.title')}></Card>
-        </Grid>
-        <Grid
-          item
-          xs={24}
-          sm={24}
-          md={24}
-          lg={15}
-          xl={15}
-          sx={{
-            height: '800px',
-          }}>
-          <Card
-            title={t('dashboard.recentOrders.title')}
-            cardContentProps={{
-              sx: {
-                height: '688px',
-              },
-            }}></Card>
-        </Grid>
-      </Grid>
+      <Stack flexDirection='row' flexWrap='wrap' gap={3} mb={3}>
+        <Card sx={{ minWidth: '20rem', flexGrow: 1 }}>
+          <CardHeader title='Itinerary' />
+          <CardContent sx={{ height: '424px' }}>
+            <form>
+              <Stack gap={1}>
+                <Autocomplete
+                  disablePortal
+                  options={placePredictions}
+                  size='small'
+                  loading={isPlacePredictionsLoading}
+                  renderInput={params => <TextField {...params} onChange={e => getPlacePredictions({ input: e.target.value })} label='Movie' />}
+                />
+                <TextField size='small' label='From' />
+                <TextField size='small' label='To' />
+                <TextField size='small' label='People' />
+                <TextField size='small' label='Nights' />
+                <TextField size='small' label='Vehicle' />
+                <TextField size='small' label='Fuel' />
+                <TextField size='small' label='Hotel' />
+                <Button type='submit' sx={{ mt: 3 }}>
+                  Start
+                </Button>
+              </Stack>
+            </form>
+          </CardContent>
+        </Card>
+        <Card sx={{ minWidth: '30rem', flexGrow: 3, height: '500px' }}>
+          <DashboardMap />
+        </Card>
+      </Stack>
     </RefineListView>
   );
 };
