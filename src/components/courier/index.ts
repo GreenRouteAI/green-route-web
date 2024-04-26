1789f8b1c72c3946504f4baa62e740cd8cba704f
NyH1409 <hei.nyhasina@gmail.com>
2024-04-26 18:59:40 +0300

    refactor: unused comps

--- src/components/courier/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './rating';
-export * from './status';
-export * from './image-upload';
-export * from './table-reviews';
--- src/components/product/index.tsx
+++
@@ -1,6 +0,0 @@
-export * from './list-table';
-export * from './list-card';
-export * from './status';
-export * from './drawer-form';
-export * from './status';
-export * from './image-upload';
